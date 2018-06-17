8cc0b6a56d4d8d6991cd595a34ae43d411f374e7
Shreddedbacon <b@benjackson.email>
2018-06-17 18:42:23 +1000

    add functionality to allow overriding config.txt to enable_uart=1

--- fileServer.go
+++ fileServer.go
@@ -5,8 +5,13 @@ import (
 	"log"
 	"net/http"
 	"path"
+  "regexp"
+  "io/ioutil"
+  "fmt"
+  "os"
 
 	"github.com/gorilla/mux"
+  "github.com/google/uuid"
 )
 
 type fileServer interface {
@@ -65,21 +70,46 @@ func (f *FileServer) fileHandler(w http.ResponseWriter, r *http.Request) {
 	//strings.Replace(bootLocation, "/", "", 1) //remove the first /
 	fullFilename := path.Join(bootLocation, filename)
 
-	if filename != "cmdline.txt" {
-		http.ServeFile(w, r, fullFilename)
-		return
-	} else {
-
+	if filename == "cmdline.txt" {
 		c := templatevars{
 			NfsServer: f.nfs.GetNfsAddress(),
 			NfsRoot:   pi.Disks[0].Location,
 		}
-
 		log.Printf("%v requested for: %v\n", filename, pi.Id)
 		t, err := template.New("templatefile").ParseFiles(templatePath+"/cmdline.txt")
 		if err != nil {
 			panic(err)
 		}
 		t.ExecuteTemplate(w, filename, c)
-	}
+	} else if filename == "config.txt" {
+      // we want to make sure we are enabling uart on rpi3s so inject into config.txt
+      // generate a new config.txt to serve temporarily from the original one without modifying it directly
+      config_file, err := ioutil.ReadFile(fullFilename) // just pass the file name
+    	if err != nil {
+    		fmt.Println(err)
+    	}
+      commentlines := regexp.MustCompile("(?m)[\r\n]+^#.*$")
+      nocomments := commentlines.ReplaceAllString(string(config_file), "")
+      pattern := regexp.MustCompile("enable_uart=[0-1]")
+    	match := pattern.FindString(string(nocomments))
+    	config_file_new := string(nocomments)
+    	if len(match) == 0 {
+    	config_file_new = string(nocomments) + "\nenable_uart=1"
+    	}
+    	config_file_save := pattern.ReplaceAllString(config_file_new, "enable_uart=1")
+      random_uuid := uuid.New().String()
+    	err = ioutil.WriteFile("/tmp/"+random_uuid, []byte(config_file_save), 0755)
+      if err != nil {
+    		fmt.Println(err.Error())
+    	}
+  		http.ServeFile(w, r, "/tmp/"+random_uuid)
+      err = os.Remove("/tmp/"+random_uuid)
+      if err != nil {
+        fmt.Println(err.Error())
+      }
+  	  return
+  } else {
+	  http.ServeFile(w, r, fullFilename)
+	  return
+  }
 }
